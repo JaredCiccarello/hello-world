f52a458fdb6cc67cb1807be027f1a28c683e67d6
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-03-24 16:39:21 -0400

    update readin 2 notes

--- script.js
+++ script.js
@@ -2,67 +2,72 @@
 // const mydiv=document.getElementById('input-prompt')
 
 
-function yourName() {
     
-    let answer = prompt('What is your name?');
-    // console.log(answer);
-    if (answer == 'no') {
-   answer = prompt ('Pretty please?')
-    } else if (answer == '') {
-        answer = prompt ('strong silent type I see')
-    } if (answer == 'fine')
-        answer = prompt ('Hoorah!')
-    document.write(answer);
-}
+    function welcomeUser(){
+    
+    let breath = prompt("Have you taken a break today? Type only Yes or No. Y E S or N O");
+                            // || (this is an OR)
+        while (breath == 'no' || breath == 'yes' || breath == 'YES' || breath == 'NO'){
 
-// let setudents = 9;
+            breath = prompt ("Remember it can only be Yes or No");
+        }
 
-// for (let i = 1 <= setudents; i++){
-//     console.log("Hello student #" + i);
-// }
-
-let myPassword = 'password'
-
-let userAnswer = prompt("What is the password?")
-
-while (userAnswer !- myPassword){
-
-    while (userAnswer != myPassword){
-        
+    if (breath == 'Yes'){
+        document.write("Great! Let's show you some extra tips on how simple changes can brighten your day!");
+    } else if (breath == 'No'){
+        document.write("I think I can help!");
+    }  else if (breath == ''){
+        document.write("That's okay, you don't have to tell me")
     }
-}
+    }
 
-// mydiv.innerHTML = answer
+    function userName
+       
+    let userName = prompt ('What should I call you?');
 
-// alert = "hello-world"
-// return answer
-
-
-// console.log(yourName());
-// let savedname = yourName()
-
-// savedname = 
+    if (usersName == ""){
+        usersName = prompt ("Hey! That's not a name!")
 
 
+    document.write(" " + usersName + "That's my mother's name actually");
+
+    console.log(usersName);
+    }
+
+
+    welcomeUser();
+
+    let newBornBabyAge = prompt ("Guess a New Born Babies age")
+
+    while (newBornBabyAge !=1){
+        newBornBabyAge = prompt ("That's not a New Born Baby's Age! Hint: It's less than 2")
+    }
+    
+    let flowers = prompt ("How many flowers do you want to see? I only have 10 I can show you")
+    
+    // function yourName() {
+        
+    //     let answer = prompt('WWhat can I call you?');
+    //     // console.log(answer);
+    //     if (answer == 'no') {
+    //    answer = prompt ('Pretty please?')
+    //     } else if (answer == '') {
+    //         answer = prompt ('strong silent type I see')
+    //     } if (answer == 'fine')
+    //         answer = prompt ('Hoorah!')
+    //     document.write(answer);
+    //     }
+    
+
+    
 
 
 
-//function timeOfDay() {
-//     let time = prompt('What hour is it? (0-23)'); // 15;
-//     let message = '';
 
-//     if (time <= 11) {
-//         message = 'Good Morning!';
-//     } else if (time <= 18) {
-//         message = 'Good Afternoon!';
-//     } else if (time < 24) {
-//         message = "Good Evening!";
-//     } else {
-//         message = "That hour does not exist!";
-//     }
 
-//     return message;
-// }
+
+
+
 
 
 
