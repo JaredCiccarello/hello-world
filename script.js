b24988b7950494b136ec74dbfff97b4014ab1c9a
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-03-24 14:16:14 -0400

    updated website

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// const mydiv=document.getElementById('input-prompt')
+
+
+function yourName() {
+    
+    let answer = prompt('What is your name?');
+    // console.log(answer);
+    if (answer == 'no') {
+   answer = prompt ('Pretty please?')
+    } else if (answer == '') {
+        answer = prompt ('strong silent type I see')
+    } if (answer == 'fine')
+        answer = prompt ('Hoorah!')
+    document.write(answer);
+}
+
+// let setudents = 9;
+
+// for (let i = 1 <= setudents; i++){
+//     console.log("Hello student #" + i);
+// }
+
+let myPassword = 'password'
+
+let userAnswer = prompt("What is the password?")
+
+while (userAnswer !- myPassword){
+
+    while (userAnswer != myPassword){
+        
+    }
+}
+
+// mydiv.innerHTML = answer
+
+// alert = "hello-world"
+// return answer
+
+
+// console.log(yourName());
+// let savedname = yourName()
+
+// savedname = 
+
+
+
+
+
+//function timeOfDay() {
+//     let time = prompt('What hour is it? (0-23)'); // 15;
+//     let message = '';
+
+//     if (time <= 11) {
+//         message = 'Good Morning!';
+//     } else if (time <= 18) {
+//         message = 'Good Afternoon!';
+//     } else if (time < 24) {
+//         message = "Good Evening!";
+//     } else {
+//         message = "That hour does not exist!";
+//     }
+
+//     return message;
+// }
+
+
+
+
